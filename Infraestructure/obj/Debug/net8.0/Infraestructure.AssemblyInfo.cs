5683eec0419a6d350ad2220255f7180dc9575914
gerpi <139481630+geraldSinertica@users.noreply.github.com>
2024-04-08 15:38:57 -0600

    add integration

--- Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
+++ Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51aef8d9805bc9af002dfe537d4a06ef47a66167")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
